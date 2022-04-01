102912585b4cc91a348790c5ef97e553b108c9a6
zc-dakshitad <dakshitad19@gmail.com>
2022-04-01 08:33:15 +0530

    added the file

--- src/test/java/EmpRecordStatusTest.java
+++ src/test/java/EmpRecordStatusTest.java
@@ -1,13 +1,11 @@
-package com.rest;
 
 import org.testng.annotations.Test;
 import utils.ConfigReader;
 import utils.ExcelUtils;
 import java.io.IOException;
 import static io.restassured.RestAssured.given;
+public class EmpRecordStatusTest {
 
-
-public class EmpRecordStatusTest<JSONObject> {
     private static String baseUrl = ConfigReader.getProperty("hostUrl");
     private static String getEmpApi =  ConfigReader.getProperty("specificEmployeeApi");
 
@@ -31,12 +29,6 @@ public class EmpRecordStatusTest<JSONObject> {
 
 
 }
-
-
-
-
-
-
 
 
 
